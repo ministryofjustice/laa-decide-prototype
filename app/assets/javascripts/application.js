0f56945196f34341e7bd43fafc99942d544739e1
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2021-07-01 20:49:04 +0100

    AD-231 Add filter component
    Add filter component to open and my applications.
    
    Doesn't actually function and screws up the layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,22 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
   window.MOJFrontend.initAll()
 })
+
+
+new MOJFrontend.FilterToggleButton({
+  bigModeMediaQuery: '(min-width: 48.063em)',
+  startHidden: true,
+  toggleButton: {
+    container: $('.moj-action-bar__filter'),
+    showText: 'Show filter',
+    hideText: 'Hide filter',
+    classes: 'govuk-button--secondary'
+  },
+  closeButton: {
+    container: $('.moj-filter__header-action'),
+    text: 'Close'
+  },
+  filter: {
+    container: $('.moj-filter')
+  }
+});
