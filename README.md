bddc53756676ffe8014229d4672f352308f081a3
Mark-Dodson-MT <Mark.Dodson@madetech.com>
2025-08-06 11:46:48 +0100

    [AD-529] Copy over Latest into v5 for future design

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # laa-decide-prototype
 Prototype for the LAA's 'Decide If An Applicant Can Get Legal Aid' service.
 
+This deploys to: 
 https://laa-decide-prototype.apps.live.cloud-platform.service.justice.gov.uk/
 
 Username: decide
+
 Password: prototype
 
+## Local Development
+
+Run `npm run dev` to spin up locally 
 
---
+++ app/routes/v5.js
@@ -0,0 +1,477 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+const DECIDED_STATES = ['granted', 'refused', 'partially granted', 'Passported']
+const NOT_DECIDED_STATES = ['Not started', 'In progress', 'rejected']
+// Add your services here
+
+const { ApplicationService } = require("../services");
+
+const find_certificate = (application, id) => {
+  for (const proceeding of application.applicationDetails.proceedings){
+    for (const cert of proceeding.certificates){
+      if (cert['id'] === id){
+        return cert
+      }
+    }
+
+  }
+  }
+// Add your routes here
+router.get('/my-applications', async function(req, res) {
+  req.session.data['request-more-information'] = '';
+
+  var refNo = req.session.data.refNo;
+  var refNoToRemove = req.session.data.refNoToRemove;
+
+//if a refNo exists then we "may" be assigning, usually only if we came from open applications
+  if (refNo != null){
+    const is_assigned = await ApplicationService.assign_application(req)
+  }
+
+  // if a refNoToRemove exists then we are un-assigning an application
+  if (refNoToRemove != null){
+    const is_unassigned = await ApplicationService.unassign_application(req)
+  }
+
+  req.session.data.refNo = null;
+  req.session.data.refNoToRemove = null;
+
+  res.render('./latest/my-applications');
+});
+
+router.get('/request-info-note', function(req, res) {
+  //this is no longer being used as it is done via the reject page
+  let application = ApplicationService.find_application(req);
+
+  // if a request for further info has been made, add an item to the application history
+  if (req.session.data['request-more-information']) {
+    application.applicationDetails.notes.push(
+        ApplicationService.create_note('You',
+        'Further information requested',
+        null ));
+    req.session.data['request-more-information'] = 'display-banner-now';
+  }
+  res.redirect('./application-details');
+});
+
+router.get('/application-details', async function(req, res)
+{
+  //update substantive proceeding merits results if have come from the merits page
+  let application = ApplicationService.find_application(req);
+  const considered_merits = await ApplicationService.update_merits_certificate_decisions(req);
+  for (const cost of application['applicationDetails']['costLimitations']) {
+    if (cost['certificateType'] === 'Substantive certificate'){
+      if (typeof req.session.data['substantiveCost'] !== 'undefined' && req.session.data['substantiveCost'] !== null) {
+        switch (req.session.data['substantiveCost']) {
+          case "Requested limit (amount)":
+            break;
+          case "Default limit (amount)":
+            cost['requestedCostLimit'] = cost['costLimit'];
+            break;
+          case "New limit set by assessor (amount)":
+            cost['requestedCostLimit'] = req.session.data['substantiveCostAmount'];
+            break;
+        }
+    }
+
+    }
+  }
+  //so update_all_substantive not being used in latest (beyond v4)
+  // merits_continue button is on both emergency and substantive pages
+  if ((req.session.data['merits_continue_button']) || (req.session.data['update_all_substantive']))
+  {
+    if (req.session.data['merits_continue_button'] != "Save and come back later")
+    {
+      const merit_result_updated = await ApplicationService.update_overall_decision(req, 'merit');
+
+      if (!NOT_DECIDED_STATES.includes(application['applicationDetails']['meritsAssessmentResult']))
+      {
+        //create an application note if merits decision made,
+        // rejected application already sends a note so not needed
+        let note_text = '';
+        for(let proceeding of application['applicationDetails']['proceedings'])
+        {
+          note_text = note_text + proceeding['proceedingType'] + '<br><p class="govuk-hint">';
+          for(let certificate of proceeding['certificates'])
+          {
+            note_text = note_text + '' + certificate['certificateType'] + ': ' + certificate['meritsResult'] + '<br>';
+          }
+          note_text = note_text + '</p>';
+        }
+
+        if (req.session.data['substantive-note'] && req.session.data['substantive-note'].length > 0)
+        {
+          note_text = note_text + 'Decision note<p class="govuk-hint">' + req.session.data['substantive-note'] + '</p>'
+        }
+
+        application.applicationDetails.notes.push(ApplicationService.create_note(
+            'You',
+            'Merits decision made',
+            note_text ));
+      }
+    }
+  }
+    req.session.data['merits_continue_button'] = '';
+    req.session.data['update_all_substantive'] = '';
+    req.session.data['granted-emergency'] = '';
+    req.session.data['emergency-certificate-start-date'] = '';
+    req.session.data['other-emergency-start-date-day'] = '';
+    req.session.data['other-emergency-start-date-month'] = '';
+    req.session.data['other-emergency-start-date-year'] = '';
+    req.session.data['emergency-certificate-end-date'] = '';
+    req.session.data['other-emergency-end-date-day'] = '';
+    req.session.data['other-emergency-end-date-month'] = '';
+    req.session.data['other-emergency-end-date-year'] = '';
+    req.session.data['emergency-note'] = '';
+    req.session.data['granted-substantive'] = '';
+    req.session.data['substantive-certificate-start-date'] = '';
+    req.session.data['other-substantive-start-date-day'] = '';
+    req.session.data['other-substantive-start-date-month'] = '';
+    req.session.data['other-substantive-start-date-year'] = '';
+    req.session.data['substantive-note'] = '';
+
+  // update proceeding means results
+
+  if ((req.session.data['means_continue_button'] === "Save decision") || (req.session.data['update_all_means']))
+  {
+    const considered_means = await ApplicationService.update_means_certificate_decisions(req);
+    //update overall means decision
+    const considered_overall_means_decision = await ApplicationService.update_overall_decision(req, 'means')
+
+    var note_text = '';
+    for(let proceeding of application['applicationDetails']['proceedings']) {
+      note_text = note_text + proceeding['proceedingType'] + ': ' + proceeding['meansResult'] + '<br>';
+    }
+
+    application.applicationDetails.notes.push(ApplicationService.create_note(
+        'You',
+        'Means decision made',
+        note_text ));
+
+    req.session.data['means_continue_button'] = '';
+    req.session.data['update_all_means'] = '';
+    req.session.data['contributionCorrect'] = '';
+    req.session.data['amended-contribution'] = '';
+    req.session.data['amended_contribution_frequency'] = '';
+    req.session.data['means-note'] = '';
+    req.session.data['means-note'] = '';
+  }
+
+  res.locals.data['application'] = application;
+  // redirect to a final decision page if necessary
+  const application_details = application['applicationDetails']
+  if (DECIDED_STATES.includes(application_details['meritsAssessmentResult']) &&
+      DECIDED_STATES.includes(application_details['meansAssessmentResult'])
+      && res.locals.data['merits_continue_button'] =='Save and continue' || res.locals.data['means_continue_button'] =='Save and continue')
+  {
+    res.render('./latest/decision-communication');
+  }
+  else{
+    res.render('./latest/application-details');}
+
+}
+);
+
+router.get('/application-history', function(req, res) {
+  res.locals.data['application'] = ApplicationService.find_application(req);
+  res.render('./latest/application-history');
+});
+
+router.get('/people', function(req, res) {
+  res.locals.data['application'] = ApplicationService.find_application(req);
+  res.render('./latest/people');
+});
+
+router.get('/decision-communication', function(req, res) {
+  let application = ApplicationService.find_application(req);
+  res.locals.data['application'] = application;
+  res.render('./latest/decision-communication');
+});
+
+router.get('/change-level-emergency', function(req, res) {
+  let application = ApplicationService.find_application(req);
+  res.locals.data['application'] = application;
+  res.locals.data['cert_to_change'] = find_certificate(application, res.locals.data['cert_id_to_change']);
+  res.render('./latest/change-level-emergency');
+});
+
+router.post('/change-emergency', function(req, res) {
+  let application = ApplicationService.find_application(req);
+  res.locals.data['application'] = application;
+  res.locals.data['cert_to_change'] = find_certificate(application, res.locals.data['cert_id_to_change']);
+  //update the emergency values
+  for (const proceeding of application['applicationDetails']['proceedings']) {
+    for (let certificate of proceeding['certificates']) {
+      if (certificate['id'] === res.locals.data['cert_id_to_change']) {
+        if (typeof req.session.data['emergencyLOS'] !== 'undefined' && req.session.data['emergencyLOS']!== null)
+        {
+          certificate['formOfService'] = req.session.data['emergencyLOS']
+        }
+        if (typeof req.session.data['emergency-scope'] !== 'undefined' && req.session.data['emergency-scope']!== null){
+          certificate['scopeLimits'] = req.session.data['emergency-scope']
+        }
+
+      }
+    }
+  }
+  req.session.data['cert_id_to_change'] = '';
+  res.render('./latest/merits-assessment-emergency');
+});
+
+router.get('/change-level-substantive', function(req, res) {
+  let application = ApplicationService.find_application(req);
+  res.locals.data['application'] = application;
+  res.locals.data['cert_to_change'] = find_certificate(application, res.locals.data['cert_id_to_change']);
+  res.render('./latest/change-level-substantive');
+});
+
+router.post('/change-substantive', function(req, res) {
+  let application = ApplicationService.find_application(req);
+  res.locals.data['application'] = application;
+  res.locals.data['cert_to_change'] = find_certificate(application, res.locals.data['cert_id_to_change']);
+  //update the values we need
+  for (const proceeding of application['applicationDetails']['proceedings']) {
+    for (let certificate of proceeding['certificates']) {
+      if (certificate['id'] === res.locals.data['cert_id_to_change']) {
+        if (typeof req.session.data['substantiveLOS'] !== 'undefined' && req.session.data['substantiveLOS']!== null)
+        {
+          certificate['formOfService'] = req.session.data['substantiveLOS']
+        }
+        if (typeof req.session.data['scope'] !== 'undefined' && req.session.data['scope']!== null){
+          certificate['scopeLimits'] = req.session.data['scope']
+        }
+
+      }
+    }
+  }
+  res.render('./latest/merits-assessment-substantive');
+});
+
+
+router.get('/merits-assessment-emergency', function(req, res) {
+  let application = ApplicationService.find_application(req);
+
+  res.locals.data['application'] = application;
+
+  if (application['applicationDetails']['certificateType'] === 'Substantive'){
+    res.render('./latest/merits-assessment-substantive');
+  }
+  else {
+    res.render('./latest/merits-assessment-emergency');
+  }
+});
+
+router.get('/merits-assessment-substantive', function(req, res) {
+  if (!["Refuse all", "Grant all"].includes(req.session.data.update_all_emergency))
+  {
+    res.locals.data['application'] = ApplicationService.find_application(req);
+    res.render('./latest/merits-assessment-substantive');
+  }
+  else
+  {// update_all_emergency is likely not used beyond v4, so put that logic below
+    if (req.session.data.update_all_emergency === 'Refuse all'){
+      res.render('./latest/refuse-application');
+    }
+    else if (req.session.data.update_all_emergency === 'Grant all')
+    {
+      let application = ApplicationService.find_application(req);
+      // grant all emergency proceeding merits results
+      for (const proceeding of application['applicationDetails']['proceedings']){
+        for (const certificate of proceeding['certificates']){
+          if (certificate['certificateType'] == 'Emergency certificate'){
+            certificate['meritsResult'] = 'granted';
+          }
+        }
+      }
+
+      application['applicationDetails']['meritsAssessmentResult'] = 'in progress';
+      res.locals.data['application'] = application;
+      res.render('./latest/merits-assessment-substantive');
+    }
+  }
+
+});
+
+router.post('/merits-assessment-substantive', async function(req, res) {
+  let application = ApplicationService.find_application(req);
+  // update emergency proceeding merits results
+  const can_continue = await ApplicationService.update_merits_certificate_decisions(req);
+  //update the costs
+  for (const cost of application['applicationDetails']['costLimitations'])
+  {
+    if (cost['certificateType'] === 'Emergency certificate')
+    {
+      if (typeof req.session.data['emergencyCost'] !== 'undefined' && req.session.data['emergencyCost'] !== null)
+      {
+        switch (req.session.data['emergencyCost'])
+        {
+          case "Requested limit (amount)":
+            break;
+          case "Default limit (amount)":
+            cost['requestedCostLimit'] = cost['costLimit'];
+            break;
+          case "New limit set by assessor (amount)":
+            cost['requestedCostLimit'] = req.session.data['emergencyCostAmount'];
+            break;
+        }
+
+      }
+    }
+  }
+  // update overall merits assessment result
+  application['applicationDetails']['meritsAssessmentResult'] = 'in progress';
+
+  res.locals.data['application'] = application;
+
+  // direct to correct page based on button clicked
+  if (req.session.data['merits_continue_button'] == "Save and come back later"){
+    res.render('./latest/application-details');
+  }
+  else {
+    res.render('./latest/merits-assessment-substantive');
+  }
+});
+
+//not used beyond version 4
+router.post('/refuse-application', function(req, res) {
+  let application = ApplicationService.find_application(req);
+
+  if (req.session.data['update_all_substantive'] === "Refuse all") {
+
+    // refuse all substantive proceeding merits results
+    for (const proceeding of application['applicationDetails']['proceedings']){
+      for (const certificate of proceeding['certificates']){
+        if (certificate['certificateType'] == 'Substantive certificate'){
+          certificate['meritsResult'] = 'refused';
+        }
+      }
+    }
+
+    application['applicationDetails']['meritsAssessmentResult'] = 'in progress'
+    res.locals.data['application'] = application;
+    res.redirect('./application-details');
+  }
+  else if (req.session.data['update_all_emergency'] === "Refuse all"){
+
+    // refuse all emergency proceeding merits results
+    for (const proceeding of application['applicationDetails']['proceedings']){
+      for (const certificate of proceeding['certificates']){
+        if (certificate['certificateType'] == 'Emergency certificate'){
+          certificate['meritsResult'] = 'refused';
+        }
+      }
+    }
+
+    application['applicationDetails']['meritsAssessmentResult'] = 'in progress';
+    res.locals.data['application'] = application;
+    res.redirect(307, './merits-assessment-substantive');
+  }
+});
+
+//not used beyond v4?
+router.get('/substantive-update-all', function(req, res) {
+  if (req.session.data.update_all_substantive === 'Refuse all'){
+    res.render('./latest/refuse-application');
+  }
+  else if (req.session.data.update_all_substantive === 'Grant all'){
+    let application = ApplicationService.find_application(req);
+
+    // grant all substantive proceeding merits results
+    for (const proceeding of application['applicationDetails']['proceedings']){
+      for (const certificate of proceeding['certificates']){
+        if (certificate['certificateType'] == 'Substantive certificate'){
+          certificate['meritsResult'] = 'granted';
+        }
+      }
+    }
+    res.locals.data['application'] = application;
+    res.redirect(307, './application-details');
+  }
+});
+
+router.post('/reject-application', async function(req, res) {
+  const application_rejected = await ApplicationService.return_application_to_provider(req)
+  res.redirect('./application-details');
+});
+
+router.post('/add-note', function(req, res) {
+  let application = ApplicationService.find_application(req);
+
+  // add an item to the application history
+
+  application.applicationDetails.notes.push(ApplicationService.create_note(
+      'You',
+      'Caseworker note added',
+      req.session.data['note']));
+  res.redirect('./application-history');
+});
+
+router.get('/means-assessment', function(req, res) {
+  let application = ApplicationService.find_application(req);
+
+  res.locals.data['application'] = application;
+
+  res.render('./latest/means-assessment');
+});
+
+
+router.get('/means-update-all', function(req, res) {
+  if (req.session.data.update_all_means === 'Refuse all'){
+    res.render('./latest/refuse-means');
+  }
+  else if (req.session.data.update_all_means === 'Grant all'){
+    let application = ApplicationService.find_application(req);
+
+    // grant all proceeding means results
+    for (const proceeding of application['applicationDetails']['proceedings']){
+      proceeding['meansResult'] = 'granted';
+    }
+    res.locals.data['application'] = application;
+    res.redirect(307, './application-details');
+  }
+});
+
+router.get('/refuse-all-means', function(req, res) {
+  let application = ApplicationService.find_application(req);
+
+  // refuse all proceeding means results
+  for (const proceeding of application['applicationDetails']['proceedings']){
+    proceeding['meansResult'] = 'refused';
+  }
+  res.locals.data['application'] = application;
+  res.redirect(307, './application-details');
+});
+
+
+router.post('/decision-check', function(request, response) {
+
+    var decisionCheck = request.session.data['overall-decision']
+    if (decisionCheck == "refuse") {
+        response.redirect("/latest/application-complete-error")
+    }  else {
+      response.redirect("/application-history")
+    }
+});
+
+
+
+// multiple add on open applications
+router.get('/add-applications', async function(req, res) {
+  req.session.data['request-more-information'] = '';
+
+  var refNo = req.session.data.refNo;
+
+//if a refNo exists then we "may" be assigning, usually only if we came from open applications
+  if (refNo != null){
+    const is_assigned = await ApplicationService.assign_application(req)
+  }
+  req.session.data.refNo = null;
+  
+  res.render('./latest/open-applications');
+});
+module.exports = router
